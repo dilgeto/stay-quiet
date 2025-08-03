89efe9ec4f4ebc3b13d90e70222ef5cbccd76178
dilgeto <cristobal.torres.u@usach.cl>
2025-08-03 18:19:28 -0400

    mejor intento

--- quiet.py
+++ quiet.py
@@ -11,11 +11,6 @@ pos = -1
 beep, samplerate = sf.read('Beep.wav', dtype='float32', start=0, stop=RATE*1)
 beep = np.mean(beep, axis=1, keepdims=True)  # Mantiene dimensión (N,1)
 
-if beep.ndim == 1:
-    beep = beep[:, np.newaxis]
-
-print(len(beep))
-
 def calcular_db(indata):
     audio_data = np.linalg.norm(indata, axis=1) if indata.ndim > 1 else indata
     rms = np.sqrt(np.mean(np.square(audio_data)))
@@ -28,25 +23,31 @@ def calcular_db(indata):
     return db
 
 def callback(indata, outdata, frames, time, status):
-    global DB, pos
+    global DB, pos, UMBRAL
     if status:
         print(status)
     DB = calcular_db(indata)
     
     if pos >= 0:
         end = pos + frames
-        #print(end)
         if end < len(beep):
             outdata[:] = beep[pos:end]
             pos = end
         else:
-            outdata[:len(beep) - pos] = beep[pos:]
-            outdata[len(beep) - pos:] = 0
-            pos = -1
+            if DB > UMBRAL:
+                remaining = len(beep) - pos
+                outdata[:remaining] = beep[pos:]
+                outdata[remaining:] = beep[:frames - remaining]
+                pos = frames - remaining
+            else :
+                remaining = len(beep) - pos
+                outdata[:remaining] = beep[pos:]
+                outdata[remaining:] = 0
+                pos = -1
     else:
         outdata.fill(0)
     
 with sd.Stream(channels=1, samplerate=RATE, callback=callback):
     while True:
-        if DB > UMBRAL:
+        if DB > UMBRAL and pos == -1:
             pos = 0
